465a8478f41dd6c981d979f1be4af83df05045aa
sarnatk <tanya200217@gmail.com>
2021-03-06 08:53:51 +0300

    Work on design

--- app/src/main/java/ru/hse/java/repetinder/LoginActivity.java
+++ app/src/main/java/ru/hse/java/repetinder/LoginActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,8 +18,8 @@ public class LoginActivity extends AppCompatActivity {
 
         Button buttonLogin = (Button) findViewById(R.id.login);
         Button buttonRegister = (Button) findViewById(R.id.register);
-        EditText editEmail = (EditText) findViewById(R.id.email);
-        EditText editPassword = (EditText) findViewById(R.id.password);
+        TextInputEditText editEmail = (TextInputEditText) findViewById(R.id.email);
+        TextInputEditText editPassword = (TextInputEditText) findViewById(R.id.password);
 
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/ru/hse/java/repetinder/RegisterActivity.java
+++ app/src/main/java/ru/hse/java/repetinder/RegisterActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,14 +16,26 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button buttonRegister = (Button) findViewById(R.id.register);
-        EditText editEmail = (EditText) findViewById(R.id.email);
-        EditText editPassword = (EditText) findViewById(R.id.password);
+        Button buttonRegister = (Button) findViewById(R.id.signUp);
+        Button buttonLogin = (Button) findViewById(R.id.backToLogin);
+        TextInputEditText editEmail = (TextInputEditText) findViewById(R.id.email);
+        TextInputEditText editFullname = (TextInputEditText) findViewById(R.id.fullname);
+        TextInputEditText editUsername = (TextInputEditText) findViewById(R.id.username);
+        TextInputEditText editPassword = (TextInputEditText) findViewById(R.id.password);
 
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
