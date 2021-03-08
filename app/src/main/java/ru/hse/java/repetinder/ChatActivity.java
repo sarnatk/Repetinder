4560e8bcf8a3181cef80919860d9047c8d364a71
German <tarabonda@bk.ru>
2021-03-08 12:45:21 +0300

    Add ChatActivity

---
+++ app/src/main/java/ru/hse/java/repetinder/ChatActivity.java
@@ -0,0 +1,25 @@
+package ru.hse.java.repetinder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class ChatActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        /*
+        Button buttonLogin = (Button) findViewById(R.id.login);
+        Button buttonRegister = (Button) findViewById(R.id.register);
+        TextInputEditText editEmail = (TextInputEditText) findViewById(R.id.email);
+        TextInputEditText editPassword = (TextInputEditText) findViewById(R.id.password);
+        */
+    }
+}
