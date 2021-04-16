98a20efe13e246113d7ab0495321a0b61481fdd2
xbreathoflife <xbreathoflife@gmail.com>
2021-04-16 13:53:43 +0300

    change user class

--- app/src/main/java/ru/hse/java/repetinder/User.java
+++ app/src/main/java/ru/hse/java/repetinder/User.java
@@ -1,13 +1,25 @@
 package ru.hse.java.repetinder;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class User {
+
+    public User(int id, String username, String password, UserRole userRole, GroupType groupType, ArrayList<Subject> subjects) {
+                this.id = id;
+                this.username = username;
+                this.password = password;
+                this.userRole = userRole;
+                this.groupType = groupType;
+                this.subjects.addAll(subjects);
+    }
 
     public enum UserRole {
         STUDENT,
         TUTOR
     }
 
-    public enum TypeClass {
+    public enum GroupType {
         SINGLE(1),
         SMALL_GROUP(5),
         MEDIUM_GROUP(15),
@@ -15,7 +27,7 @@ public class User {
 
         private final int quantity;
 
-        TypeClass(int quantity) {
+        GroupType(int quantity) {
             this.quantity = quantity;
         }
 
@@ -24,10 +36,11 @@ public class User {
         }
     }
 
-    public enum TypeOnline {
+    public enum OnlineStatus {
         ONLINE,
-        OFFLINE_STUDENT,
-        OFFLINE_TUTOR
+        OFFLINE;
+
+        private final Date lastOnlineTime = new Date(System.currentTimeMillis());
     }
 
     public enum Subject {
@@ -50,11 +63,27 @@ public class User {
         HISTORY
     }
 
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public GroupType getGroupType() {
+        return groupType;
+    }
+
+    public ArrayList<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public int getId() {
+        return id;
+    }
+
     private int id;
     private String username;
     private String password;
     private UserRole userRole;
-    private TypeClass typeClass;
-    private TypeOnline typeOnline;
-    private Subject subject;
+    private GroupType groupType;
+    private OnlineStatus typeOnline;
+    private ArrayList<Subject> subjects;
 }
